c23dea404a70816fdcab398cee61ca092092ef69
Mary Mazi <mazi.mary.o@gmail.com>
2018-01-22 09:15:37 +0100

    Testing git webhook for jenkins

--- app/client/components/home-page/HomePage.jsx
+++ app/client/components/home-page/HomePage.jsx
@@ -90,7 +90,7 @@ export class HomePage extends React.Component {
                 <div className="row">
                   <div className="col s12 m10 offset-m1 center">
                     <header className="index-header">
-                      <h2 className="light"><b>Welcome to PostIT</b></h2>
+                      <h2 className="light"><b>Welcome to PostIT!</b></h2>
                     </header>
                     <div className="auth">
                       <h4 className="index-line-height">
